4f70f45e1f7f4f8649f25bb52ab1764a88321470
mvavilin <realvavilin@yandex.ru>
2026-02-19 16:27:42 +0300

    chore: add Prettier rules and scripts

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+export default {
+  tabWidth: 2,
+  bracketSameLine: true,
+  arrowParens: 'always',
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  semi: true,
+  useTabs: false,
+  endOfLine: 'auto',
+  printWidth: 100,
+};

feb8253dabf28535ef6d17f1394fd7c525c14227
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-02-20 17:58:17 +0300

    chore: base component decomposition cleaning

--- apps/client/src/types/components/button.ts
+++
@@ -1,8 +0,0 @@
-import type { BaseComponentProps } from './base';
-
-export type ButtonComponentProps = {
-  type?: 'button' | 'submit' | 'reset';
-  disabled?: boolean;
-  name?: string;
-  value?: string;
-} & BaseComponentProps;
--- apps/client/src/types/components/form.ts
+++
@@ -1,6 +0,0 @@
-import type { BaseComponentProps } from './base';
-
-export type FormComponentProps = {
-  action?: string;
-  method?: 'get' | 'post';
-} & BaseComponentProps;

1485ed8935e16efa8c3822afea1ea7fa7cc53d51
mvavilin <realvavilin@yandex.ru>
2026-02-21 18:00:03 +0300

    test: deploy server and frontend 3

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
 
 export default defineConfig({
   plugins: [tsconfigPaths(), tailwindcss()],
+
+  root: path.resolve(__dirname),
 
   server: {
     port: 3000,
@@ -13,5 +16,8 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'main.ts'),
+    },
   },
 });

a0d78ff3053e8e2f4dbec512f6ff060f0f5a03a8
Walle908 <walle0908@yandex.ru>
2026-02-28 00:22:49 +0400

    fix: correct due to changes

--- apps/client/src/pages/RegistrationPage/RegistrationPage.ts
+++ apps/client/src/pages/RegistrationPage/RegistrationPage.ts
@@ -5,68 +5,27 @@ import {
   FormComponent,
   HeadingComponent,
   InputComponent,
+  LabelComponent,
   TextComponent,
 } from '@/api/ComponentsAPI';
 import { inputEmailInfo, inputNameInfo, inputPasswordInfo } from '../../constants/input.constants';
 import type { inputBlockType } from '@/types/registration.types';
 
-// const sectionStyles =
-//   'relative w-screen h-screen bg-[url("src/assets/bg-sky.png")] bg-center bg-cover bg-no-repeat';
+const sectionStyles =
+  'relative w-screen h-screen bg-[url("src/assets/bg-sky.png")] bg-center bg-cover bg-no-repeat';
 
-const sectionStyles = [
-  'relative',
-  'w-screen',
-  'h-screen',
-  'bg-[url("src/assets/bg-sky.png")]',
-  'bg-center',
-  'bg-cover',
-  'bg-no-repeat',
-];
-const formStyles = [
-  'fixed',
-  'top-1/2',
-  'left-1/2',
-  '-translate-x-1/2',
-  '-translate-y-1/2',
-  'w-100',
-  'p-15',
-  'bg-gray/85',
-  'rounded-xl',
-  'flex',
-  'flex-col',
-  'justify-center',
-  'items-center',
-  'gap-6',
-  'm-0',
-];
-const titleStyle = ['text-3xl', 'font-bold', 'text-yellow', 'font-brand'];
-const containerStyles = ['w-full', 'flex', 'flex-col', 'self-center', 'gap-2'];
-const inputStyles = [
-  'p-2',
-  'bg-white/40',
-  'border',
-  'border-solid',
-  'border-black',
-  'rounded-md',
-  'hover:cursor-pointer',
-  'hover:transition-colors',
-  'hover:duration-300',
-  'focus:border-yellow',
-  'outline-none',
-];
-const labelStyles = ['uppercase', 'font-medium'];
-const buttonStyles = [
-  'bg-gray-blue',
-  'w-36',
-  'h-9',
-  'rounded-md',
-  'font-medium',
-  'hover:cursor-pointer',
-  'hover:bg-soothig-green',
-  'hover:transition-colors',
-  'hover:duration-300',
-];
-const spanStyles = ['text-red'];
+const formStyles =
+  'fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-80 p-12 md:w-96 md:p-14 bg-gray/85 rounded-xl flex flex-col justify-center items-center gap-6 m-0';
+
+const titleStyle = 'text-3xl font-bold text-yellow font-brand';
+const containerStyles = 'w-full flex flex-col self-center gap-2';
+const inputStyles =
+  'p-2 bg-white/40 border border-solid border-black rounded-md hover:cursor-pointer hover:transition-colors hover:duration-300 focus:border-yellow outline-none';
+
+const labelStyles = 'uppercase font-medium';
+const buttonStyles =
+  'bg-gray-blue w-36 h-9 rounded-md font-medium hover:cursor-pointer hover:bg-soothing-green hover:transition-colors hover:duration-300';
+const spanStyles = 'text-red';
 
 export default class RegistrationPage extends BaseComponent {
   constructor() {
@@ -106,9 +65,7 @@ export default class RegistrationPage extends BaseComponent {
     if (minLength && maxLength) {
       input.setAttributes({ minLength, maxLength });
     }
-
-    const label = new BaseComponent({ tag: 'label', classes: labelStyles, content: labelText });
-    label.setAttributes({ for: id });
+    const label = new LabelComponent({ classes: labelStyles, content: labelText, htmlFor: id });
     const span = new TextComponent({ tag: 'span', content: 'Error', classes: spanStyles });
     if (container instanceof ContainerComponent) {
       container.appendChildren([label, input, span]);

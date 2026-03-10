d2719605d7a97ee73bae0e8d2b4da198dfdef58d
sergey-ado <adonisiy2@gmail.com>
2026-03-10 23:49:07 +0400

    feat: add types for Room and socketEvents

---
+++ packages/shared/src/socketEvents.ts
@@ -0,0 +1,25 @@
+import type { RoomPreview, RoomSettings } from './types/room.ts';
+
+type ClientEvent =
+  | { type: 'room:create'; payload: { settings: RoomSettings } }
+  | { type: 'room:ask-list' };
+
+type ServerEvent =
+  | { type: 'session:token'; payload: { sessionToken: string } }
+  | { type: 'room:send-list'; payload: { roomPreviews: RoomPreview[] } };
+
+type EventName<T> = T extends { type: infer K } ? K : never;
+
+type EventPayload<T> = T extends { payload: infer P }
+  ? P extends undefined
+    ? () => void
+    : (data: P) => void
+  : () => void;
+
+type DiscriminatedUnionToSocketEvents<T extends { type: string }> = {
+  [K in EventName<T>]: EventPayload<Extract<T, { type: K }>>;
+};
+
+export type ServerToClientEvents = DiscriminatedUnionToSocketEvents<ServerEvent>;
+
+export type ClientToServerEvents = DiscriminatedUnionToSocketEvents<ClientEvent>;
